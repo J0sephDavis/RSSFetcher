e0b617dab5d2f931e7cf5c6121877649dbb53ffc
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2024-08-09 11:38:43 -0400

    Prototype some of the classes & add XML notes

---
+++ Murong Xue/FeedData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Murong_Xue
+{
+    internal class FeedData
+    {
+        protected string _sTitle { get; set; }
+        protected string _sFileName { get; set; }
+        protected Uri _URL { get; set; } //TODO how to use this Uri data type
+        protected string _sexpression { get; set; }
+        protected string _sHistory { get; set; }
+
+        FeedData() //accept RSS data here?
+        {
+
+        } 
+    }
+}
--- Murong Xue/Program.cs
+++ Murong Xue/Program.cs
@@ -1,1 +1,49 @@
-﻿Console.WriteLine("Start.");
+﻿//Feed class
+// - link to XML resource
+// - DownloadXML()
+/* From C21.03, format of our local data
+ * <xml ...> //HEADER
+ * <root>
+ * 	<item>
+ * 		<title>title of the feed or whatever - this is probably just for us</title>
+ * 		<feedFileName>FileName</feedFileName>
+ * 		<feed-url><![CDATA[https://examples.com/RSS]]</feed-url>
+ * 		<expr>regex here</expre>
+ * 		<history>title of the last downloaded rss entry</history>
+ * 	</item>
+ * 	...ad infinitum
+ * </root>
+ */
+/* Library options for handling XML feeds
+ * - XmlDocument (https://learn.microsoft.com/en-us/dotnet/api/system.xml.xmldocument?view=net-8.0)
+ *  1. Handles the document In-Memory (more memory used than stream-based solutions)
+ *  2. "Represents an XML document. You can use this class to load, validate, edit, add,
+ *  and position XML in a document".
+ * - XmlReader
+ *  1. Stream-based
+ *  2. "Represents a reader that provides fast, noncached, forward-only access to XML data".
+ * - XmlWriter
+ *  1. Stream-based
+ *  2. "Represents a writer that provides a fast, non-cached, forward-only way to generate streams
+ *  or files that contain XML data".
+ */
+/* Notes on handling XML
+ * 1. XMLReader for handling the content feeds
+ * 2. XMLDocument for handling our own data
+ * 3. I doubt it is of concern, but it will be worth taking into
+ *  consideration how much memory is used for an XMLDocument and whether we should
+ *   3.1. Use an XML Reader to load data at the beginning & use an XMLDocument at the end to make changes
+ *   3.2. Use an XML Document throughout the entire lifetime of the program to manage our data
+ *   3.3. Use an XMLReader to load data at the beginning, and have a ToString() option for each FeedData which is used in the XMLWriter
+ * 
+ */
+using System.ComponentModel.DataAnnotations;
+
+namespace MurongXue;
+public class Program
+{
+    public void Main(string[] args)
+    {
+        Console.WriteLine("MAIN");
+    }
+}
