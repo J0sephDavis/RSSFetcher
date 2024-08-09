c9605f2fea7d46b6fa2fdbb8a8b9a339c6a8357e
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2024-08-09 12:30:04 -0400

    Create constructor(WIP) & print method for FeedData

--- Murong Xue/FeedData.cs
+++ Murong Xue/FeedData.cs
@@ -8,15 +8,28 @@ namespace Murong_Xue
 {
     internal class FeedData
     {
-        protected string _sTitle { get; set; }
-        protected string _sFileName { get; set; }
-        protected Uri _URL { get; set; } //TODO how to use this Uri data type
-        protected string _sexpression { get; set; }
-        protected string _sHistory { get; set; }
+        protected string Title { get; set; }
+        protected string FileName { get; set; }
+        protected Uri URL { get; set; } //TODO how to use this Uri data type
+        protected string Expression { get; set; }
+        protected string History { get; set; }
 
-        FeedData() //accept RSS data here?
+        public FeedData() //accept RSS data here?
         {
+            this.Title = "subspleases - slime";
+            this.FileName = "subsplease_tensei_shitara.xml";
+            this.URL = new Uri("https://nyaa.si/?page=rss&q=1080+%5BSubsPlease%5D+Tensei+Shitara+Slime+Datta+Ken+-&c=0_0&f=0");
+            this.Expression = "[SubsPlease] Tensei Shitara Slime Datta Ken -.*";
+            this.History = "[SubsPlease] Tensei Shitara Slime Datta Ken - 65 (1080p) [7C636E3C].mkv";
+        }
 
-        } 
+        public void Print()
+        {
+            Console.WriteLine($"Title: {this.Title}\t" +
+                $"FileName:{this.FileName}\t" +
+                $"URL:{this.URL}\t" +
+                $"Expr:{this.Expression}\t" +
+                $"History:{this.History}");
+        }
     }
 }
