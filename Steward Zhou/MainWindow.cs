504457f84b94c417eed196adc485c4439b89177d
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2024-08-21 12:05:56 -0400

    Form1->MainWindow

---
+++ Steward Zhou/MainWindow.cs
@@ -0,0 +1,15 @@
+namespace Steward_Zhou
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
