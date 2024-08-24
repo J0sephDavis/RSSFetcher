0e11396d64186eaf93fc126364aaf299a7ce9e9e
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2024-08-24 12:39:23 -0400

    winform- split view horizontal sep fill each panel

--- Steward Zhou/MainWindow.Designer.cs
+++ Steward Zhou/MainWindow.Designer.cs
@@ -30,35 +30,63 @@
         {
             FeedTree = new TreeView();
             LogListBox = new ListBox();
+            splitContainer1 = new SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
             SuspendLayout();
             // 
             // FeedTree
             // 
-            FeedTree.Location = new Point(12, 12);
+            FeedTree.Dock = DockStyle.Fill;
+            FeedTree.Location = new Point(0, 0);
             FeedTree.Name = "FeedTree";
-            FeedTree.Size = new Size(341, 426);
+            FeedTree.Size = new Size(800, 225);
             FeedTree.TabIndex = 0;
             // 
             // LogListBox
             // 
+            LogListBox.Dock = DockStyle.Fill;
             LogListBox.FormattingEnabled = true;
             LogListBox.HorizontalScrollbar = true;
             LogListBox.ItemHeight = 15;
-            LogListBox.Location = new Point(359, 12);
+            LogListBox.Location = new Point(0, 0);
             LogListBox.Name = "LogListBox";
-            LogListBox.Size = new Size(429, 424);
+            LogListBox.Size = new Size(800, 221);
             LogListBox.TabIndex = 1;
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.Location = new Point(0, 0);
+            splitContainer1.Name = "splitContainer1";
+            splitContainer1.Orientation = Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(FeedTree);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(LogListBox);
+            splitContainer1.Size = new Size(800, 450);
+            splitContainer1.SplitterDistance = 225;
+            splitContainer1.TabIndex = 2;
             // 
             // MainWindow
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(LogListBox);
-            Controls.Add(FeedTree);
+            Controls.Add(splitContainer1);
             Name = "MainWindow";
             Text = "MainWindow";
             Load += MainWindow_Load;
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
@@ -66,5 +94,6 @@
 
         private TreeView FeedTree;
         private ListBox LogListBox;
+        private SplitContainer splitContainer1;
     }
 }
