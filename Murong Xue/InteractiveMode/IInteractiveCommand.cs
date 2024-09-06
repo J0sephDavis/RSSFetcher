2d327d73bb3abf0789b374d3d183ed2c12e50e4e
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2024-09-06 17:52:35 -0400

    extract InteractiveCommand interface to its own file

---
+++ Murong Xue/InteractiveMode/IInteractiveCommand.cs
@@ -0,0 +1,17 @@
+﻿namespace Murong_Xue.InteractiveMode
+{
+    internal interface IInteractiveCommand
+    {
+        /// <summary>
+        /// processes the command with the given arguments
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        int Handle(string[] args);
+        /// <summary>
+        /// resturns the command name, e.g., "print"
+        /// </summary>
+        /// <returns></returns>
+        string GetName();
+    }
+}
--- Murong Xue/InteractiveMode/InteractiveEditor.cs
+++ Murong Xue/InteractiveMode/InteractiveEditor.cs
@@ -5,20 +5,6 @@ using RSSFetcher.Logging.Reporting;
 
 namespace Murong_Xue.InteractiveMode
 {
-    internal interface IInteractiveCommand
-    {
-        /// <summary>
-        /// processes the command with the given arguments
-        /// </summary>
-        /// <param name="args"></param>
-        /// <returns></returns>
-        int Handle(string[] args);
-        /// <summary>
-        /// resturns the command name, e.g., "print"
-        /// </summary>
-        /// <returns></returns>
-        string GetName();
-    }
 
     internal class InteractiveEditor
     {
