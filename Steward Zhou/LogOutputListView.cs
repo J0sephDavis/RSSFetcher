dbdb35bcf962a38d4a496131c3c15bd547713f02
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2024-09-06 13:20:51 -0400

    LogOutputListView stops writing when element is disposed (could be a race condition?)

--- Steward Zhou/LogOutputListView.cs
+++ Steward Zhou/LogOutputListView.cs
@@ -1,24 +1,26 @@
 ﻿using Murong_Xue.Logging;
 using Murong_Xue.Logging.OutputHandling;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Steward_Zhou
 {
     internal class LogOutputListView : IOutputModule
     {
         ListBox lb;
+        bool Disposed = false;
         public LogOutputListView(ListBox ListBox)
         {
             lb = ListBox;
+            lb.Disposed += OnDisposed;
+        }
+
+        private void OnDisposed(object? sender, EventArgs e)
+        {
+            Disposed = true;
         }
         public void WriteMsg(LogMsg msg, bool InteractiveMode)
         {
-        //TODO implement a try catch that just dumps it incase a log is being put through
-        //when the app is closing (happened) Or somehow check if the winform is closed/being closed
+            if (Disposed) return;
+
             if (lb.InvokeRequired)
                 lb.Invoke((MethodInvoker)delegate
                 {
