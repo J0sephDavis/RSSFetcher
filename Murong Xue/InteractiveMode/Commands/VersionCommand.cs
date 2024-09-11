9ecb1d7fcadb6527114034185c47a5677665d01c
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2024-09-11 16:40:30 -0400

    create version command

---
+++ Murong Xue/InteractiveMode/Commands/VersionCommand.cs
@@ -0,0 +1,25 @@
+﻿using RSSFetcher.Logging.Reporting;
+using static RSSFetcher.InteractiveMode.InteractiveEditor;
+
+namespace RSSFetcher.InteractiveMode.Commands
+{
+    internal class VersionCommand(Controller control, InteractiveReporter report) : IInteractiveCommand(control, report)
+    {
+        static readonly string[] valid_commands = ["version"];
+        public override string GetHelp() => "no args, gets version information";
+
+        public override string GetName() => valid_commands[0];
+        public override bool CommandMatch(string command)
+        {
+            foreach (string cmd in valid_commands)
+                if (cmd == command)
+                    return true;
+            return false;
+        }
+        public override INTERACTIVE_RESPONSE Handle(string[] args)
+        {
+            report.Interactive(Controller.versionString);
+            return INTERACTIVE_RESPONSE.SUCCESS;
+        }
+    }
+}
