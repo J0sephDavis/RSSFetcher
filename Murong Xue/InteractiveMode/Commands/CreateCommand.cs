a810d4edd88fdc799428dd7b8dc2faf8706b3452
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2024-09-11 15:45:53 -0400

    prototype create command

---
+++ Murong Xue/InteractiveMode/Commands/CreateCommand.cs
@@ -0,0 +1,23 @@
+﻿using RSSFetcher.Logging.Reporting;
+using static RSSFetcher.InteractiveMode.InteractiveEditor;
+
+namespace RSSFetcher.InteractiveMode.Commands
+{
+    internal class CreateCommand(Controller control, InteractiveReporter report) : IInteractiveCommand(control, report)
+    {
+        static readonly string[] valid_commands = ["create", "new"];
+        public override string GetName() => valid_commands[0];
+        public override bool CommandMatch(string command)
+        {
+            foreach (string cmd in valid_commands)
+                if (cmd == command)
+                    return true;
+            return false;
+        }
+
+        public override INTERACTIVE_RESPONSE Handle(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
