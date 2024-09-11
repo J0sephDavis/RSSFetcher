5ba8543b8fc9397e8916e1ebc202a2220e7f9c2c
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2024-09-11 12:11:34 -0400

    Reduce RSSFetcher::Program.cs's responsibility. Use new Controller & CommandLineArguments

--- Murong Xue/Program.cs
+++ Murong Xue/Program.cs
@@ -1,35 +1,24 @@
-﻿using Murong_Xue.InteractiveMode;
-using Murong_Xue.Logging.Output.Modules;
+﻿using RSSFetcher.InteractiveMode;
+using RSSFetcher.Logging.Output.Modules;
 using RSSFetcher.FeedData;
 using RSSFetcher.Logging;
 using RSSFetcher.Logging.Reporting;
+using System.Text;
 
 namespace RSSFetcher;
-public class Program
+public partial class Program
 {
-    static readonly int MAJOR_VERSION = 1;
-    static readonly int MINOR_VERSION = 7;
-    static readonly int PATCH = 0;
+    public static readonly int MAJOR_VERSION = 1;
+    public static readonly int MINOR_VERSION = 7;
+    public static readonly int PATCH = 0;
     //---
-    static Reporter report; //cannot be readonly. Must be set AFTER Config has been created
-    static readonly EventTicker events = EventTicker.GetInstance();
-
-    [Flags]
-    protected enum ArgResult
+    static Reporter report;
+    public static void Main(string[] args)
     {
-        NONE    = 0,
-        EXIT    = 1 << 0,
-        EDIT    = 1 << 1,
-        RUN     = 1 << 2,
-    };
-    public static async Task Main(string[] args)
-    {
-        using Config cfg = Config.GetInstance();
-        report ??= Logger.RequestReporter("PROGRM");
-        Logger.GetInstance().LogConsole();
+        report = Logger.RequestReporter("PROGRM");
         report.Notice($"VERSION {MAJOR_VERSION}.{MINOR_VERSION}.{PATCH}");
 #if DEBUG
-        report.Debug("!! PROGRAM COMPILED IN DEBUG MODE !!");
+        report.Trace("compilation flag DEBUG was set");
         Logger.SetLogLevel(new(LogType.ALL, LogMod.ALL));
         args = [
             //"--version"
@@ -41,158 +30,9 @@ public class Program
             "--edit",
         ];
 #endif
-        ArgResult choice = HandleArgs(cfg, args);
-        report.DebugVal($"Choice: {choice}");
-        switch (choice)
-        {
-            case (ArgResult.EDIT):
-                StartInteractive();
-                break;
-            case (ArgResult.RUN):
-                //await RSSEntries.Process();
-                throw new NotImplementedException();
-                break;
-            case (ArgResult.NONE):
-            default:
-                goto case ArgResult.EXIT;
-            case (ArgResult.EXIT):
-                report.Trace("ArgResult.EXIT");
-                return;
-        }
-        report.Out(events.GetSummary());
+        using Controller controller = new(new(args));
+        controller.Run();
+        report.Out(controller.GetSummary());
         report.Notice("Program STOP");
     }
-    public static void StartInteractive()
-    {
-        report.Trace("StartInteractive");
-        InteractiveEditor editor = new();
-        editor.MainLoop();
-        report.Trace("interactive mode ended");
-    }
-
-    private static ArgResult HandleArgs(Config cfg, string[] args)
-    {
-#if DEBUG
-        report.Trace($"HandleArgs provided with {args.Length}args");
-        foreach (string s in args)
-            report.TraceVal(s);
-#endif
-        bool NextIsConfig = false;
-        bool NextIsDownloadDir = false;
-        bool SetLogLevel = false;
-        LogLevel level = new(LogType.DEFAULT, LogMod.DEFAULT);
-        ArgResult retVal = ArgResult.RUN;
-        report.Notice("Processing Args");
-        foreach (string arg in args)
-        {
-            if (NextIsConfig)
-            {
-                cfg.SetRSSPath(Path.GetFullPath(arg));
-                //---
-                NextIsConfig = false;
-                continue;
-            }
-            if (NextIsDownloadDir)
-            {
-                cfg.SetDownloadPath(Path.GetFullPath(arg));
-                //---
-                NextIsDownloadDir = false;
-                continue;
-            }
-            //-------------------------
-            string _arg = arg.ToLower();
-            if (help_cmds.Contains(_arg))
-            {
-                report.Out(help_str);
-                retVal = ArgResult.EXIT;
-                continue;
-            }
-            if (version_cmds.Contains(_arg))
-            {
-                report.Out($"VERSION {MAJOR_VERSION}.{MINOR_VERSION}.{PATCH}");
-                retVal = ArgResult.EXIT;
-                continue;
-            }
-            if (edit_cmds.Contains(_arg))
-            {
-                retVal = ArgResult.EDIT;
-                continue;
-            }
-            //-------------------------
-            switch (_arg)
-            {
-                case ("--spam"):
-                    level |= LogMod.SPAM;
-                    SetLogLevel = true;
-                    break;
-                case ("--verbose"):
-                    level |= LogMod.VERBOSE;
-                    SetLogLevel = true;
-                    break;
-                case ("--unimportant"):
-                    level |= LogMod.UNIMPORTANT;
-                    SetLogLevel = true;
-                    break;
-#if DEBUG
-                case ("--debug"):
-                    level |= LogType.DEBUG;
-                    break;
-#endif
-                default:
-                    break;
-            }
-            //-------------------------
-            if (rss_cmds.Contains(_arg))
-            {
-                NextIsConfig = true;
-                continue;
-            }
-            if (download_cmds.Contains(_arg))
-            {
-                NextIsDownloadDir = true;
-                continue;
-            }
-        }
-        //---
-        if (SetLogLevel)
-        {
-            report.Out($"Masking log level {level}");
-            Logger.MaskLogLevel(level);
-        }
-        report.Log(LogType.DEBUG, LogMod.UNIMPORTANT, $"HandleArgs returning: {retVal}");
-        return retVal;
-    }
-    #region Static strings for arguments
-    //---------- STATIC STRINGS -----------
-    protected static readonly string[] version_cmds = ["--version", "-version", "-v", "--v"];
-    protected static readonly string version_cmd_desc = "(void) Show version information";
-
-    protected static readonly string[] edit_cmds = ["-edit", "--edit"];
-    protected static readonly string edit_cmd_desc = "(void) Enter interactive mode and edit the rss config";
-
-    protected static readonly string[] rss_cmds = ["-rsscfg", "--rsscfg"];
-    protected static readonly string rss_cmd_desc = "(string) Set the path of the RSS Config file";
-
-    protected static readonly string[] download_cmds = ["-downloadpath", "--downloadpath"];
-    protected static readonly string download_cmd_desc = "(string) Set the download folder for all fetched files";
-
-    protected static readonly string log_cmd_desc = "Enable the following log flags:" +
-        "\n\t--SPAM" +
-        "\n\t--VERBOSE" +
-        "\n\t--UNIMPORTANT"
-#if DEBUG
-        + "\n\t--DEBUG"
-#endif
-        ;
-
-    protected static readonly string[] help_cmds = ["-help", "--help", "-h", "--h"];
-    protected static readonly string help_cmd_desc = "(void) Get a brief description for each command";
-    protected static readonly string help_str =
-        $"{help_cmds[0]}:\t{help_cmd_desc}\n" +
-        $"{version_cmds[0]}:\t{version_cmd_desc}\n" +
-        $"{edit_cmds[0]}:\t{edit_cmd_desc}\n" +
-        $"{rss_cmds[0]}:\t{rss_cmd_desc}\n" +
-        $"{download_cmds[0]}:\t{download_cmd_desc}\n" +
-        $"{log_cmd_desc}";
 }
-#endregion
