801d053436065a60902d112822a5192e82ce6890
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2024-09-12 18:15:03 -0400

    Refactor DataFile ReadFeeds to use serializer and xml attribute class

--- RSSFetcher/FeedData/DataFile.cs
+++ RSSFetcher/FeedData/DataFile.cs
@@ -1,18 +1,114 @@
 ﻿using RSSFetcher.Logging;
 using RSSFetcher.Logging.Reporting;
 using RSSFetcher.Summary;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata.Ecma335;
 using System.Xml;
+using System.Xml.Serialization;
 
 namespace RSSFetcher.FeedData
 {
-    public class DataFile(Uri RSSPath) : ISummarizeable
+    partial class DataFile : ISummarizeable
     {
-        private readonly Uri path = RSSPath;
-        private readonly Reporter report = Logger.RequestReporter("ENTDAT");
         // --- Summary Data
         private int Loaded = 0;
         private int Written = 0;
         private readonly List<TimeSpan> ages = [];
+        // ---
+        string ISummarizeable.Name => "DataFile";
+        List<SummaryItem> ISummarizeable.GetSummary()
+        {
+            List<SummaryItem> items = [];
+            // ---
+            items.Add(new("Loaded", Loaded.ToString()));
+            items.Add(new("Written", Written.ToString()));
+            //---
+            if (ages.Count > 0)
+            {
+                TimeSpan average = TimeSpan.Zero;
+                TimeSpan max = TimeSpan.Zero;
+                foreach (TimeSpan t in ages)
+                {
+                    if (t > max) max = t;
+                    average += t;
+                }
+                average = average.Divide(ages.Count);
+                items.Add(new("Oldest (days)", max.TotalDays.ToString()));
+                items.Add(new("Average (days)", average.TotalDays.ToString()));
+            }
+            // ---
+            return items;
+        }
+    }
+    
+    //[SerializableAttribute()]
+    [XmlTypeAttribute("item",AnonymousType = true)]
+    [XmlRootAttribute("root",IsNullable = false)]
+    public partial class xmlRoot
+    {
+        private xmlFeed[] feeds;
+        [XmlElementAttribute("item")]
+        public xmlFeed[] item
+        {
+            get => this.feeds;
+            set => this.feeds = value;
+        }
+    }
+
+    //[SerializableAttribute()]
+    [XmlTypeAttribute(AnonymousType = true)]
+    public partial class xmlFeed
+    {
+        private string titleField;
+        private string feedurlField;
+        private string exprField;
+        private string historyField;
+        private string dateField;
+
+        public string title
+        {
+            get => this.titleField;
+            set => this.titleField = value;
+        }
+
+        [XmlElementAttribute("feed-url")]
+        public string feedurl
+        {
+            get => this.feedurlField;
+            set => this.feedurlField = value;
+        }
+        public string expr
+        {
+            get => this.exprField;
+            set => this.exprField = value;
+        }
+        public string history
+        {
+            get => this.historyField;
+            set => this.historyField = value;
+        }
+        public string date
+        {
+            get => this.dateField;
+            set => this.dateField = value;
+        }
+        public Feed ToFeed()
+        {
+            return new()
+            {
+                Title = title,
+                URL = new(feedurl),
+                Expression = expr,
+                History = history,
+                Date = DateTime.Parse(date),
+                Status = FeedStatus.INIT | FeedStatus.FROM_FILE,
+            };
+        }
+    }
+    public partial class DataFile(Uri RSSPath)
+    {
+        private readonly Uri path = RSSPath;
+        private readonly Reporter report = Logger.RequestReporter("ENTDAT");
         //--------------------------------------------------------------------
         private const string RSS_Title = "title";
         private const string RSS_URL = "feed-url";
@@ -20,124 +116,34 @@ namespace RSSFetcher.FeedData
         private const string RSS_History = "history";
         private const string RSS_Item = "item";
         private const string RSS_Date = "date";
-
-        string ISummarizeable.Name => "DataFile";
-
         //--------------------------------------------------------------------
         //! Reads the XML files and populated the FeedEntry list
         public List<Feed> ReadFeeds()
         {
+            List<Feed> feeds = [];
+            // ---
             report.Trace($"Reading from {path}");
-            List<Feed> Feeds = [];
-            /* NOTE! Running the XMLReader in Async on our config file takes 23-26ms
-             * Running Synchronously it takes 13-14ms                               */
             if (File.Exists(path.LocalPath) == false)
             {
                 report.Error($"RSS Config File not found ({path})");
-                return Feeds;//TODO throw an err
+                return feeds;//TODO throw an err
             }
 
             using FileStream xStream = File.Open(path.LocalPath, FileMode.Open);
 
             XmlReaderSettings xSettings = new() { Async = false };
             using XmlReader reader = XmlReader.Create(xStream, xSettings);
+            // ---
+            XmlSerializer serializer = new XmlSerializer(typeof(xmlRoot));
+            xmlRoot? document = serializer.Deserialize(reader) as xmlRoot;
+            //
+            if (document == null)
+                throw new ApplicationException("Failed to deserialize xml");
 
-            Feed? feed = null;
-            bool InTitle = false;
-            bool InUrl = false;
-            bool InExpr = false;
-            bool InHistory = false;
-            bool InDate = false;
-            DateTime DateNow = DateTime.Now;
-            while (reader.Read())
-            {
-#pragma warning disable CS8602 // Dereference of a possibly null reference. (Feed? feed)
-                switch (reader.NodeType)
-                {
-                    case XmlNodeType.Element:
-                        switch (reader.Name)
-                        {
-                            case RSS_Title:
-                                InTitle = true;
-                                break;
-                            case RSS_URL:
-                                InUrl = true;
-                                break;
-                            case RSS_Expression:
-                                InExpr = true;
-                                break;
-                            case RSS_History:
-                                InHistory = true;
-                                break;
-                            case RSS_Date:
-                                InDate = true;
-                                break;
-                            case RSS_Item:
-                                feed = new();
-                                break;
-                            default:
-                                break;
-                        }
-                        break;
-                    case XmlNodeType.Text:
-                        if (InTitle)
-                        {
-                            feed.Title = reader.Value;
-                        }
-                        else if (InExpr)
-                        {
-                            feed.Expression = reader.Value;
-                        }
-                        else if (InHistory)
-                        {
-                            feed.History = reader.Value;
-                        }
-                        else if (InDate)
-                        {
-                            feed.Date = DateTime.Parse(reader.Value);
-                        }
-                        break;
-                    case XmlNodeType.EndElement:
-                        switch (reader.Name)
-                        {
-                            case RSS_Title:
-                                InTitle = false;
-                                break;
-                            case RSS_URL:
-                                InUrl = false;
-                                break;
-                            case RSS_Expression:
-                                InExpr = false;
-                                break;
-                            case RSS_History:
-                                InHistory = false;
-                                break;
-                            case RSS_Date:
-                                InDate = false;
-                                break;
-                            case RSS_Item:
-                                feed.Status |= FeedStatus.FROM_FILE;
-                                Feeds.Add(feed);
-                                Loaded++;
-                                ages.Add(DateNow - feed.Date);
-                                break;
-                            default:
-                                break;
-                        }
-                        break;
-                    case XmlNodeType.CDATA:
-                        if (InUrl)
-                        {
-                            feed.URL = new(reader.Value);
-                        }
-                        break;
-                    default:
-                        break;
-                }
-#pragma warning restore CS8602 // Dereference of a possibly null reference.
-            }
-
-            return Feeds;
+            foreach (var item in document.item)
+                feeds.Add(item.ToFeed());
+            // ---
+            return feeds;
         }
         private void RenameOldFile()
         {
@@ -162,7 +168,7 @@ namespace RSSFetcher.FeedData
             using (XmlWriter writer = XmlWriter.Create(xStream, xSettings))
             {
                 //-------- ROOT
-                writer.WriteStartElement(null, "root", null);
+                writer.WriteStartElement(null, "xmlRoot", null);
                 TimeSpan SinceLast;
                 ages.Clear();
                 //---- item
@@ -197,30 +203,6 @@ namespace RSSFetcher.FeedData
                 writer.WriteEndElement();
                 writer.Flush();
             }
-        }
-
-        List<SummaryItem> ISummarizeable.GetSummary()
-        {
-            List<SummaryItem> items = [];
-            // ---
-            items.Add(new("Loaded",Loaded.ToString()));
-            items.Add(new("Written", Written.ToString()));
-            //---
-            if (ages.Count > 0)
-            {
-                TimeSpan average = TimeSpan.Zero;
-                TimeSpan max = TimeSpan.Zero;
-                foreach (TimeSpan t in ages)
-                {
-                    if (t > max) max = t;
-                    average += t;
-                }
-                average = average.Divide(ages.Count);
-                items.Add(new("Oldest (days)", max.TotalDays.ToString()));
-                items.Add(new("Average (days)", average.TotalDays.ToString()));
-            }
-            // ---
-            return items;
         }
     }
 }
